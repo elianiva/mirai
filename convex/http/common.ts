f003a4046c9e17eb7eb952630600ca62b7f8b131
elianiva <git@elianiva.my.id>
2025-06-18 22:16:15 +0700

    fix: specify cors

--- convex/http/common.ts
+++ convex/http/common.ts
@@ -1,5 +1,5 @@
 export const CORS_HEADERS = new Headers({
-	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Origin": "https://mirai.elianiva.my.id",
 	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 	"Access-Control-Allow-Headers":
 		"Content-Type, Authorization, X-Requested-With, Accept, Origin, Digest",
